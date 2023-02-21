39465fb39e26c988c0df3bba16ecca77ef9f4ea9
Cmbailey089 <christopherbailey089@gmail.com>
2023-02-20 21:27:59 -0500

    added more javascript

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
+window.onload =function(){
+
 var questionBox = document.getElementsByClassName("Question")
-var answerbox = document.getElementsByClassName("button")
+var answerBox = document.getElementsByClassName("answers")
 var checker = document.getElementsByClassName("checker")
 var current = 0
 
@@ -20,3 +22,59 @@ function loadQuestion(curr) {
     questionBox.innerHtml= question;
 }
 
+function loadAnswers(curr) {
+    var answers = questionnaire[Object.keys(questionnaire)[curr]];
+
+    answerBox.innerHtml="";
+
+    for(var i=0;i<answers.length-1;i+=1){
+        var createDiv= document.createElement("div"),
+        text=document.createTextNode(answers[i]);
+
+        createDiv.appendChild(text);
+        createDiv.addEventListener("click",checkAnswer(i,answers));
+        answerBox.appendChild(createDiv);
+    }
+}
+
+function checkAnswer(i,arr) {
+    return function(){  
+        var userAnswer=arr[arr.length-1];
+
+        if (userAnswer===correctAnswer){
+            checker(true);
+        } else {
+            checker(false);
+        }
+
+        if (current<Object.keys(questionnaire).length-1){
+            current+=1;
+
+            loadQuestion(current);
+            loadAnswers(current);
+        } else {
+            questionBox.innerHtml= "Your score";
+            answerBox.innerHtml="";
+        }
+    };
+}
+
+function checker(boolean) {
+    var createDiv=document.createElement("div"),
+    text = document.createTextNode(current+1);
+
+    createDiv.appendChild(text);
+
+    if(boolean) {
+        createDiv.className+="Correct";
+        checker.appendChild(createDiv);
+    } else {
+        createDiv.className+="Wrong";
+        checker.appendChild(createDiv);
+    }
+}
+
+loadQuestion(current);
+loadAnswers(current);
+
+}
